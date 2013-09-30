d613fb127c1104700533071e9cb09873e03f2975
nickniebaum <nickniebaum@gmail.com>
2013-09-30 11:10:15 -0500

    seo content

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -15,3 +15,12 @@
 	<?php echo form_submit('send','Send Message') ?>
 </div>
 <?php echo form_close() ?>
+<div style="height:0px;overflow:hidden">
+	<a href="http://lifthousedesign.com/">Austin Web Design</a> company, Lift House Design, offers 
+	<a href="http://lifthousedesign.com/site/web_development">Web Development</a>,
+	<a href="http://lifthousedesign.com/site/website_design">Website Design</a>,
+	<a href="http://lifthousedesign.com/site/search_engine_optimization">Search Engine Optimization</a>,
+	<a href="http://lifthousedesign.com/site/graphic_design">Graphic Design</a>,
+	<a href="http://lifthousedesign.com/site/photography">Photography</a>,
+	<a href="http://lifthousedesign.com/site/videography">Videography</a>, and Voice Acting services
+</div>
